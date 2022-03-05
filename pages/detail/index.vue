5d1a6e730105da5a218a61e1f804fb1780c7754d
yuki yamagata <yshanxing4@gmail.com>
2022-03-06 02:49:04 +0900

    spanタグの改行

--- pages/detail/index.vue
+++ pages/detail/index.vue
@@ -9,14 +9,16 @@
 
           <div class="table-row">
             <div class="table-cell py-10 whitespace-nowrap  text-center border-b-2">
-              タスク名<span class="ml-15">:</span>
+              タスク名
+              <span class="ml-15">:</span>
             </div>
             <div class="table-cell pl-30 border-b-2">Netflixで気になるアニメを朝まで見続ける</div>
           </div>
 
           <div class="table-row">
             <div class="table-cell text-center whitespace-nowrap pt-10 pb-30 border-b-2">
-              内容<span class="ml-20">:</span>
+              内容
+              <span class="ml-20">:</span>
             </div>
             <div class="table-cell pl-30 border-b-2">
               最近、忙しくてアニメを見る時間がないのでここらでひたすらアニメを見る時間が必要だと思う。 眠たむなっても大丈夫なようにmosterと眠眠打破を用意する。 もし、寝落ちしてしまったら、もう一度初めから見直す。
@@ -25,14 +27,16 @@
 
           <div class="table-row">
             <div class="table-cell text-center border-b-2 py-10 whitespace-nowrap">
-              ステータス<span class="ml-13">:</span>
+              ステータス
+              <span class="ml-13">:</span>
             </div>
             <div class="table-cell border-b-2 pl-30">未着手</div>
           </div>
 
           <div class="table-row">
             <div class="table-cell tex-center whitespace-nowrap border-b-2 py-10">
-              優先度<span class="ml-20">:</span>
+              優先度
+              <span class="ml-20">:</span>
             </div>
             <div class="table-cell border-b-2 pl-30">高</div>
           </div>
